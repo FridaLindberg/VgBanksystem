9dfd74e3e6f2682000a7920cfee0d5796f17082d
Frida Lindberg <fridalindberg03@gmail.com>
2022-12-22 14:12:50 +0100

    initial commit

---
+++ src/Utility/History.java
@@ -0,0 +1,27 @@
+package Utility;
+
+import Users.User;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+public class History {
+    File file = new File("resources/history.txt");
+
+    public void writeToFile(String text, User user) {
+        try(BufferedWriter print = new BufferedWriter(new FileWriter(file, true))){
+
+            String textForHistoryTextFile = "User: "+ user.getName() + ".\n" +
+                    "Activity: " + text + ".\n" +
+                    "Time: " + LocalTime.now().withNano(0) + ".\n" +
+                    "Date: " + LocalDate.now() + "\n\n";
+            print.write(textForHistoryTextFile);
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+}
